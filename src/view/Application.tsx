2a8e0a0b88ca4f8d596a09e656175c17bc144249
Sergey Kot <bytekot@gmail.com>
2022-10-16 19:44:28 +0300

    Update layout

--- src/view/Application.tsx
+++ src/view/Application.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CVView from './CV';
-import { basicInfo, skills, jobs } from '../cv.json';
+import { general, skills, jobs } from '../cv.json';
 
-const ApplicationView = () => <CVView basicInfo={basicInfo} skills={skills} jobs={jobs} />
+const ApplicationView = () => <CVView general={general} skills={skills} jobs={jobs} />
 
 export default ApplicationView;
