f5e75240328ac300a23eaa83ac8bb7d39466eaa3
Sergey Kot <bytekot@gmail.com>
2022-10-30 04:16:12 +0300

    Jobs theme fix

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import { CSS_PREFIX } from '../constants';
 import { JobData } from '../types';
 
 const Job = ({ company, title, startDate, endDate, description }: JobData): JSX.Element =>
-    <div key={company}>
+    <div className={`${CSS_PREFIX}job`} key={company}>
         <strong>{company}</strong><br />
         {title}<br />
-        {startDate} — {endDate}
+        ({startDate} — {endDate})
         <p>{description}</p>
     </div>
 
